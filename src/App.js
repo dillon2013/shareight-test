bd0b20e70ff939d7645844f0d3595c139483c699
Dillon Lee <dillon2013@users.noreply.github.com>
2018-06-12 22:52:43 +0100

    basic filter functionality and routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import data from './data/products';
+
+
+function Home () {
+  return (
+    <div>
+      <p>this is the home page</p>
+    </div>
+  )
+}
+
+function Product ({match}) {
+  return (
+    <div>
+      {data.products.filter(product => product.type === match.params.productType).map(product => (
+        <p key={product.name}>{ product.name }</p>
+      ))}
+    </div>
+  )
+}
+
+function ProductLanding (props) {
+  return (
+    <div>Click on the category to filter</div>
+  )
+}
+
+function Products ({match}) {
+  let productTypes = ['dresses', 'jeans', 'shoes', 't-shirts'];
+  return (
+    <div>
+        <ul>
+          {productTypes.map(productType => (
+            <Link key={productType} to={`/products/${productType}`}>
+              <li>{ productType }</li>
+            </Link>
+          ))}
+        </ul>
+        <Route exact path={match.path} component={ProductLanding} />
+        <Route path={`${match.path}/:productType`} component={Product} />
+    </div>
+  )
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/products" component={Products}/>
+          </Switch>
       </div>
+      </Router>
     );
   }
 }
